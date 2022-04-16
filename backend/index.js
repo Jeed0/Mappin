cd7d2b9fa9b3032c2a2e4e9bff88e9f778faddc8
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2022-04-16 18:48:59 -0400

    First commit mongo db ok

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology : true,
+}).then(()=>{
+    console.log("MongoDB Connecté!")
+}).catch(err=> console.log(err));
+
+app.listen(8800,()=>{
+    console.log("backend server est en marche!")
+})
+
+//  "start" : "nodemon index.js" à la place de test dans -> pckjson
