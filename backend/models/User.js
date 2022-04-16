5cb3f03f6c31db0271b923c4ee744e65e160709f
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2022-04-16 19:13:29 -0400

    creation des modeles

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        require: true,
+        min: 3,
+        max: 20,
+        unique: true, // un seul nom par utilisateur
+    },
+    email:{
+        type: String,
+        max: 50,
+        unique: true,
+    },
+    password: {
+        type: String, 
+        required,
+        min: 6,
+    },
+    },
+    { timestamps: true },
+);
+
+module.exports = mongoose.model("User", UserSchema);
